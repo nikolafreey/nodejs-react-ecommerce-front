3cedb1c7a6a0122102cb40b360c7ae109e028e5b
Nikola Vukovic <nikolav54@gmail.com>
2021-06-17 13:49:11 +0200

    Added redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -9,17 +9,42 @@ import Register from "./pages/auth/Register";
 import RegisterComplete from "./pages/auth/RegisterComplete";
 import Home from "./pages/Home";
 
-const App = () => (
-  <>
-    <Header />
-    <ToastContainer />
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/register" component={Register} />
-      <Route exact path="/register/complete" component={RegisterComplete} />
-    </Switch>
-  </>
-);
+import { useDispatch } from "react-redux";
+import { auth } from "./firebase";
+import userEvent from "@testing-library/user-event";
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  //to check firebase auth state
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const idTokenResult = await user.getIdTokenResult();
+
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: { email: user.email, token: idTokenResult.token },
+        });
+      }
+    });
+
+    //cleanup
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <ToastContainer />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/register/complete" component={RegisterComplete} />
+      </Switch>
+    </>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,13 +5,20 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import "antd/dist/antd.css";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
+
+//store
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  // </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
 
