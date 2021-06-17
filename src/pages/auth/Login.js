b816b8617dd5686f51727cb5e421212560d12ca6
Nikola Vukovic <nikolav54@gmail.com>
2021-06-17 15:37:01 +0200

    Added login with Google

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { toast } from "react-toastify";
-import { auth } from "../../firebase";
+import { auth, googleAuthProvider } from "../../firebase";
 import { Button } from "antd";
-import { MailOutlined } from "@ant-design/icons";
+import { GoogleOutlined, MailOutlined } from "@ant-design/icons";
 import { useDispatch } from "react-redux";
 import { LOGGED_IN_USER } from "../../actionTypes/userActionTypes";
 
@@ -33,6 +33,24 @@ const Login = ({ history }) => {
     }
   };
 
+  const googleLogin = () => {
+    auth
+      .signInWithPopup(googleAuthProvider)
+      .then(async (result) => {
+        const { user } = result;
+        const idTokenResult = await user.getIdTokenResult();
+        dispatch({
+          type: LOGGED_IN_USER,
+          payload: { email: user.email, token: idTokenResult.token },
+        });
+        history.push("/");
+      })
+      .catch((error) => {
+        toast.error(error.message);
+        console.error(error);
+      });
+  };
+
   const loginForm = () => (
     <form onSubmit={handleSubmit}>
       <div className="form-group">
@@ -58,7 +76,7 @@ const Login = ({ history }) => {
       <Button
         onClick={handleSubmit}
         type="primary"
-        class="mb-3"
+        className="mb-3"
         icon={<MailOutlined />}
         block
         shape="round"
@@ -74,8 +92,22 @@ const Login = ({ history }) => {
     <div className="container p-5">
       <div className="row">
         <div className="col-md-6 offset-md-3">
-          <h4>Login</h4>
+          {!loading ? (
+            <h4>Login</h4>
+          ) : (
+            <h4 className="text-danger">Loading. . .</h4>
+          )}
           {loginForm()}
+          <Button
+            onClick={googleLogin}
+            type="danger"
+            icon={<GoogleOutlined />}
+            block
+            shape="round"
+            size="large"
+          >
+            Login with Google
+          </Button>
         </div>
       </div>
     </div>
