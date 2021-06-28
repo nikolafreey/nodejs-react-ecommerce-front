d7055a97b8cc5ee77302b397580eb2abdc2ab712
Nikola Vukovic <nikolav54@gmail.com>
2021-06-29 01:34:05 +0200

    Added Password reset functionality and History and WIshlist pages

---
+++ src/pages/user/Password.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import UserNav from "../../components/nav/UserNav";
+import { toast } from "react-toastify";
+import { auth } from "../../firebase";
+
+const Password = () => {
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    await auth.currentUser
+      .updatePassword(password)
+      .then(() => {
+        setLoading(false);
+        setPassword("");
+        toast.success("Password updated!");
+      })
+      .catch((err) => {
+        setLoading(false);
+        toast.error(err.message);
+      });
+  };
+
+  const passwordUpdateForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label>Your Password</label>
+        <input
+          type="password"
+          onChange={(event) => setPassword(event.target.value)}
+          className="form-control"
+          placeholder="Enter New Password"
+          disabled={loading}
+          value={password}
+        />
+        <button
+          className="btn btn-primary"
+          disabled={!password || password.length < 6 || loading}
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-2">
+          <UserNav />
+        </div>
+        <div className="col">
+          {loading ? (
+            <h4 className="text-danger">Loading. . .</h4>
+          ) : (
+            <h4>Password Update</h4>
+          )}
+          {passwordUpdateForm()}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Password;
--- src/pages/user/WIshlist.js
+++ src/pages/user/WIshlist.js
@@ -1,17 +1,17 @@
 import React from "react";
 import UserNav from "../../components/nav/UserNav";
 
-const History = () => {
+const Wishlist = () => {
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-md-2">
           <UserNav />
         </div>
-        <div className="col">User History Page</div>
+        <div className="col">User Wishlist Page</div>
       </div>
     </div>
   );
 };
 
-export default History;
+export default Wishlist;
