bd047e2ec08b0a32041677de5947f630a21b3ef1
Nikola Vukovic <nikolav54@gmail.com>
2021-08-22 22:00:58 +0200

    Added stripe checkout and payment

---
+++ src/components/StripeCheckout.js
@@ -0,0 +1,154 @@
+import React from "react";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { useSelector, useDispatch } from "react-redux";
+import { createPaymentIntent } from "../services/stripeService";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Card } from "antd";
+import placeholder from "../images/Screenshot_111.png";
+import { CheckOutlined, DollarOutlined } from "@ant-design/icons";
+
+const StripeCheckout = ({ history }) => {
+  const dispatch = useDispatch();
+  const { user, coupon } = useSelector((state) => ({ ...state }));
+
+  const [succeeded, setSucceeded] = useState(false);
+  const [error, setError] = useState(null);
+  const [processing, setProcessing] = useState("");
+  const [disabled, setDisabled] = useState(true);
+  const [clientSecret, setClientSecret] = useState("");
+  const [cartTotal, setCartTotal] = useState(0);
+  const [totalAfterDiscount, setTotalAfterDiscount] = useState(0);
+  const [payable, setPayable] = useState(0);
+
+  const stripe = useStripe();
+  const elements = useElements();
+
+  useEffect(() => {
+    createPaymentIntent(user.token, coupon).then((res) => {
+      setClientSecret(res.data.clientSecret);
+      //additional response recieved on successful payment
+      setCartTotal(res.data.cartTotal);
+      setTotalAfterDiscount(res.data.totalAfterDiscount);
+      setPayable(res.data.payable);
+    });
+  }, []);
+
+  const cardStyle = {
+    style: {
+      base: {
+        color: "#32325d",
+        fontFamily: "Arial, sans-serif",
+        fontSmoothing: "antialiased",
+        fontSize: "16px",
+        "::placeholder": {
+          color: "#32325d",
+        },
+      },
+      invalid: {
+        color: "#fa755a",
+        iconColor: "#fa755a",
+      },
+    },
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setProcessing(true);
+
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: e.target.name.value,
+        },
+      },
+    });
+
+    if (payload.error) {
+      setError(`Payment failed ${payload.error.message}`);
+      setProcessing(false);
+    } else {
+      //here you get the result after successful payment
+      //create order and save in database for admin to process and empty user cart from Redux and localStorage
+      console.log(JSON.stringify(payload, null, 4));
+      setError(null);
+      setProcessing(false);
+      setSucceeded(true);
+    }
+  };
+
+  const handleChange = async (e) => {
+    //listen for changes in the card element and display and errors as the customer puts in card details
+    setDisabled(e.empty); //disable pay button if there is any errors
+    setError(e.error ? e.error.message : ""); //show the error message
+    console.log("handleChange", e);
+  };
+
+  return (
+    <>
+      {!succeeded && (
+        <div>
+          {coupon && !totalAfterDiscount ? (
+            <p className="alert alert-success">{`Total After Discount: $${totalAfterDiscount}`}</p>
+          ) : (
+            <p className="alert alert-danger">No Coupon Applied!</p>
+          )}
+        </div>
+      )}
+      <div className="text-center pb-5">
+        <Card
+          cover={
+            <img
+              src={placeholder}
+              alt="Slika"
+              style={{
+                height: "200px",
+                objectFit: "cover",
+                marginBottom: "-50px",
+              }}
+            />
+          }
+          actions={[
+            <>
+              <DollarOutlined className="text-info" /> <br /> Total: $
+              {cartTotal}
+            </>,
+            <>
+              <CheckOutlined className="text-info" /> <br /> Total Payable: $
+              {(payable / 100).toFixed(2)}
+            </>,
+          ]}
+        />
+      </div>
+      <form id="payment-form" className="stripe-form" onSubmit={handleSubmit}>
+        <CardElement
+          id="card-element"
+          options={cardStyle}
+          onChange={handleChange}
+        />
+        <button
+          className="stripe-button"
+          disabled={processing || disabled || succeeded}
+        >
+          <span id="button-text">
+            {processing ? <div className="spinner" id="spinner"></div> : "Pay"}
+          </span>
+        </button>
+        <br />
+        {error && (
+          <div className="card-error" role="alert">
+            {error}
+          </div>
+        )}
+        <br />
+        <p className={succeeded ? "result-message" : "result-message hidden"}>
+          Payment Successful.{" "}
+          <Link to="/user/history">See it in your purchase history.</Link>
+        </p>
+      </form>
+    </>
+  );
+};
+
+export default StripeCheckout;
